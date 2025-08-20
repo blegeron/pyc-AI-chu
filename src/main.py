a80b96ef958e125f8d99599d52fa8fcf1fc83d08
Baptiste Legeron <baptiste.legeron@gmail.com>
2025-08-20 18:33:05 +0200

    work

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
-def main():
-    print("Hello from poke-agent!")
+import click
+from loguru import logger
+
+
+@click.command()
+@click.option(
+    "--name",
+    help="The person to greet.",
+)
+def main(name: any) -> None:
+    """Say hello to NAME."""
+    click.echo(f"Hello {name}!")
+    logger.info("✅ Hello executed.")
 
 
 if __name__ == "__main__":
